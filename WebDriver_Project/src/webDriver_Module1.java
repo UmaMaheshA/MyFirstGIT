2ca14b4afd862b56d36ff2ecf6063569e5da1faf
UmaMaheshA <umamahesh.aum@gmail.com>
2017-08-27 13:58:07 -0700

    New Code updated...

--- WebDriver_Project/src/webDriver_Module1.java
+++ WebDriver_Project/src/webDriver_Module1.java
@@ -6,9 +6,12 @@ public class webDriver_Module1 {
 	
 	
 	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver" , "E:\\geckodriver.exe");
 		
 		WebDriver F_Browser = new  FirefoxDriver();
+		F_Browser.get("http://perfmen.in");
 		
+		System.out.println(" hey mahesh....");
 		
 	}
 
